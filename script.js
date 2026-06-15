db2d225fd54f0b0c90afa703cec85657346a4c45
patilvrushali2507-dot <patilvrushali2507@gmail.com>
2026-06-15 17:46:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+window.onscroll = function () {
+    const navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+};
